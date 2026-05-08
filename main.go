ca53f9a13d3a852e7f2df94444e205b0b2cbdb87
naghara15 <naghara15@gmail.com>
2026-05-08 23:47:55 +0700

    feat: tambah koneksi database

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"chefskiss-backend/config"
+)
+
 func main() {
-	
+	fmt.Println("Mencoba koneksi ke database PostgreSQL...")
+
+	config.ConnectDatabase()
+	fmt.Println("Mantap! Test koneksi database sukses.")
 }
